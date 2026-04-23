92d840cbedf8c959a6c33d26c3b2412431eb0724
dpthibeault99 <dpthibeault@email.neit.edu>
2026-04-23 10:29:46 -0400

    resets to middle

---
+++ js/game.js
@@ -0,0 +1,64 @@
+var canvas;
+var context;
+var player;
+var ball;
+var timer;
+var interval = 1000 / 60; // 60 fps
+
+canvas = document.getElementById("myCanvas");
+context = canvas.getContext("2d");
+
+// the paddle
+player = new gameObject(10, 0 , 30, 150, "#ff00f2");
+player.vx = 0;
+player.vy = 0;
+
+// the ball
+ball = new Ball(canvas, context);
+ball.moveBall();
+
+timer = setInterval(animate, interval);
+
+function animate()
+{
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        // player movement
+        if (w)
+        {
+                player.y -= 4;
+        }
+
+        if (s)
+        {
+                player.y += 4;
+        }
+
+        boundry();
+
+        if (ball.collisionCheck(player))
+        {
+                ball.x = player.right() + ball.radius;
+                ball.speedX = Math.abs(ball.speedX);
+                ball.color = "#ffff00";
+        }
+
+        player.drawRect();
+        player.move();
+
+        ball.drawBall();
+        ball.update();
+}
+
+function boundry()
+{
+    if (player.y > canvas.height - player.height / 2)
+    {
+        player.y = canvas.height - player.height / 2;
+    }
+
+    if (player.y < player.height / 2)
+    {
+        player.y = player.height / 2;
+    }
+}
